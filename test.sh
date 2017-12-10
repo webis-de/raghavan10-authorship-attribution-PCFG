67df4b937606020de3ee8316f838764036b0b246
Roland Schmid <roland.schmid@in.tum.de>
2017-12-10 13:49:50 +0100

    add evaluator

--- test.sh
+++ test.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-. init_environment.sh && python3 raghavan10.py -i pan12-authorship-attribution-test-dataset-problem-a-2015-10-20/ -o output
+
+inputDataset="pan12-authorship-attribution-test-dataset-problem-a-2015-10-20/"
+output="output/"
+
+. init_environment.sh && python3 raghavan10.py -i $inputDataset -o $output
+
+octave -q authorship_attribution_eval.m -a $output/answers.json -t $inputDataset/ground-truth.json -m $inputDataset/meta-file.json -o $output/evaluation.prototext
